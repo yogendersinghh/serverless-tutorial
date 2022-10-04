7d41c5c1e07408b3ee228b2840b757e24d8e1774
yogender singh <yogender.singh4343@gmail.com>
2022-10-04 15:15:40 +0530

    serverless crud api

--- src/api/api.js
+++ src/api/api.js
@@ -1,90 +1,98 @@
 const mysql = require("serverless-mysql")({
-    config:{
-        host:process.env.HOST,
-        database:process.env.DB,
-        user:process.env.USER,
-        password:process.env.PSWD
-    }
-})
-module.exports.testing = async(event)=>{
-    console.log("connection established",process.env.HOST)
-    let value = "select * from student"
-    const sql =  await mysql.query(value)
-    const response = {
-        statusCode : 200,
-        body:JSON.stringify(sql,
-        null,
-        2)
-    }
-    return response
-}
+  config: {
+    host: process.env.HOST,
+    database: process.env.DB,
+    user: process.env.USER,
+    password: process.env.PSWD,
+  },
+});
+module.exports.testing = async (event) => {
+  let value = "select * from Contacts";
+  const sql = await mysql.query(value);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(sql, null, 2),
+  };
+  return response;
+};
 
-module.exports.postApi = async(event)=>{
-    console.log("post api",event)
-      const data =   JSON.parse(event.body)
-      const {name,age,number,country,id} = data;
-    let createQuery = "insert into student values(?,?,?,?,?)"
-    let sql = await mysql.query(createQuery,[name,age,number,country,id])
-    const response = {
-        statuscCode:200,
-        body:JSON.stringify({
-            message:"hey this is post api calling function",
-            input:event
-        },
-        null,
-        2)
-    }
-    return response
-}
+module.exports.createApi = async (event) => {
+  const data = JSON.parse(event.body);
+ 
+  const { fname, mname, lname, country } = data;
+  let createQuery = "insert into Contacts SET ?";
+  let sql = await mysql.query(createQuery, data);
+ 
+  const response = {
+    statuscCode: 200,
+    body: JSON.stringify(
+      {
+        message: "hey this is post api calling function",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+  return response;
+};
 
-module.exports.updateApi = async(event)=>{
-    const paramsID = event.pathParameters
-    const data = JSON.parse(event.body)
-    console.log("🚀 ~ file: api.js ~ line 43 ~ module.exports.updateApi=async ~ data", data)
-    const {name,age,number,country,id} = data
-    console.log("🚀 ~ file: api.js ~ line 45 ~ module.exports.updateApi=async ~ age",name)
-    let updateQuery  = 'update student set name="'+name+'",age="'+age+'",number="'+number+'",country="'+country+'",id="'+id+'" where id=?';
-    await mysql.query(updateQuery,[paramsID],(err,result)=>{
-        if (err) throw err
-        console.log("result",result)
-        let response = {
-            statusCode:200,
-            body:JSON.stringify({
-                message:"data is updated",
-                input:result
-            })
-        }
-        return response
-    })
-    // console.log("🚀 ~ file: api.js ~ line 46 ~ module.exports.updateApi=async ~ sql", sql)
-}
+module.exports.createOrg = async (event) => {
+  let data = await JSON.parse(event.body);
+  let query = "insert into Organization SET ?";
+  await mysql.query(query, data);
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Org is creted",
+      input: event,
+    }),
+  };
+  return response;
+};
 
-module.exports.getOne = async(event)=>{
-    // console.log("evenet",)
-    let {id} = event.pathParameters
-    let getOneQuery = "select * from student where id= ?"
-    let sql =  await mysql.query(getOneQuery,[id])
+module.exports.updateApi = async (event) => {
+  const paramsID = event.pathParameters;
+  const data = JSON.parse(event.body);
+  let updateQuery = "update Contacts SET ? where ?";
+  const result = await mysql.query(updateQuery, [data,paramsID])
     let response = {
-        statusCode:200,
-        body:JSON.stringify(sql,null,2)
-    }
-    console.log("🚀 ~ file: api.js ~ line 63 ~ module.exports.getOne=async ~ response", response)
-    return response
-}
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "data is updated",
+        input: result,
+      }),
+    };
+      mysql.end();
+    return response;
+};
 
+module.exports.getOne = async (event) => {
+  let { id } = event.pathParameters;
+  let getOneQuery = "select * from Contacts where idContacts= ?";
+  let sql = await mysql.query(getOneQuery, [id]);
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify(sql, null, 2),
+  };
+  
+  return response;
+};
 
-module.exports.deleteOne = async(event)=>{
-    const {id} = event.pathParameters;
-    let query = "delete from student where id=?";
-    let sql = await mysql.query(query,[id])
-    console.log("🚀 ~ file: api.js ~ line 80 ~ module.exports.deleteOne ~ sql", sql)
-    let response = {
-        statusCode:200,
-        body:JSON.stringify({
-            message:"user is deleted",
-            input:sql
-        },null,2)
-    }
-    return response
-    
-}
+module.exports.deleteOne = async (event) => {
+  const paramsID= event.pathParameters;
+  let query = "delete from Contacts where ?";
+  let sql = await mysql.query(query, paramsID);
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "user is deleted",
+        input: sql,
+      },
+      null,
+      2
+    ),
+  };
+  return response;
+};
