4be684565e02daa56099abdda23fe2aaf09ef711
yogender singh <yogender.singh4343@gmail.com>
2022-10-08 20:29:47 +0530

    serverless api routes updates

--- src/api/api.js
+++ src/api/api.js
@@ -6,48 +6,122 @@ const mysql = require("serverless-mysql")({
     password: process.env.PSWD,
   },
 });
-module.exports.testing = async (event) => {
+module.exports.getAll = async (event) => {
   let value = "select * from Contacts";
   const sql = await mysql.query(value);
   const response = {
     statusCode: 200,
     body: JSON.stringify(sql, null, 2),
+    data: sql,
   };
   return response;
 };
 
-module.exports.createApi = async (event) => {
+module.exports.getAllOrg = async (event) => {
+  let value = "select * from Organization";
+  const sql = await mysql.query(value);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(sql, null, 2),
+    data: sql,
+  };
+  return response;
+};
+
+module.exports.createContact = async (event, context) => {
   const data = JSON.parse(event.body);
- 
-  const { fname, mname, lname, country } = data;
+  let { FirstName, Organizatin_idOrganization } = data;
+  let response = {
+    statusCode: 0,
+    body: "",
+  };
+  if (!FirstName && !Organizatin_idOrganization) {
+    (response.statusCode = 400),
+      (response.body = JSON.stringify(
+        { message: "please fill ateast First Name And Org ID" },
+        null,
+        2
+      ));
+    return response;
+  }
+  let query = "select * from Organization where idOrganization = ?";
+  let res = await mysql.query(query, [data.Organizatin_idOrganization]);
+  if (!res.length) {
+    (response.statusCode = 404),
+      (response.body = JSON.stringify(
+        { message: "No Org Id Exists. Please Create a Org ID first" },
+        null,
+        2
+      ));
+    return response;
+  }
+  let ContactsQuery =
+    "select * from Contacts where Organizatin_idOrganization = ?";
+  let result = await mysql.query(ContactsQuery, [
+    data.Organizatin_idOrganization,
+  ]);
+  if (result.length) {
+    (response.statusCode = 403),
+      (response.body = JSON.stringify(
+        { message: `Contact with This Org ID ${Organizatin_idOrganization} is already exists`},
+        null,
+        2
+      ));
+
+    return response;
+  }
   let createQuery = "insert into Contacts SET ?";
   let sql = await mysql.query(createQuery, data);
- 
-  const response = {
-    statuscCode: 200,
-    body: JSON.stringify(
+  (response.statusCode = 200),
+    (response.body = JSON.stringify(
       {
-        message: "hey this is post api calling function",
-        input: event,
+        message: "Contact Created Successfully",
+        input: sql,
       },
       null,
       2
-    ),
-  };
+    ));
+
   return response;
 };
 
 module.exports.createOrg = async (event) => {
   let data = await JSON.parse(event.body);
-  let query = "insert into Organization SET ?";
-  await mysql.query(query, data);
+  let { OrganizationName, Email } = data;
   let response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Org is creted",
-      input: event,
-    }),
+    statusCode: 0,
+    body: "",
   };
+  if (!OrganizationName && !Email) {
+    response.statusCode = 400;
+    response.body = JSON.stringify(
+      { message: "please fill ateast Email And OrganizationName" },
+      null,
+      2
+    );
+    return response
+  }
+  let EmailQuery = "select * from Organization where Email = ?";
+  let result = await mysql.query(EmailQuery, [Email]);
+    console.log("🚀 ~ file: api.js ~ line 94 ~ module.exports.createOrg= ~ result", result)
+    if (result.length) {
+    (response.statusCode = 403),
+      (response.body = JSON.stringify(
+        { message: `Organization with This Email ${Email} is already exists` },
+        null,
+        2
+      ));
+
+    return response;
+  }
+  let query = "insert into Organization SET ?";
+  let sql = await mysql.query(query, data);
+  mysql.end();
+  (response.statusCode = 200),
+    (response.body = JSON.stringify({
+      message: "Org is creted",
+      input: sql,
+    }));
   return response;
 };
 
@@ -55,16 +129,16 @@ module.exports.updateApi = async (event) => {
   const paramsID = event.pathParameters;
   const data = JSON.parse(event.body);
   let updateQuery = "update Contacts SET ? where ?";
-  const result = await mysql.query(updateQuery, [data,paramsID])
-    let response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "data is updated",
-        input: result,
-      }),
-    };
-      mysql.end();
-    return response;
+  const result = await mysql.query(updateQuery, [data, paramsID]);
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "data is updated",
+      input: result,
+    }),
+  };
+  mysql.end();
+  return response;
 };
 
 module.exports.getOne = async (event) => {
@@ -75,12 +149,12 @@ module.exports.getOne = async (event) => {
     statusCode: 200,
     body: JSON.stringify(sql, null, 2),
   };
-  
+
   return response;
 };
 
-module.exports.deleteOne = async (event) => {
-  const paramsID= event.pathParameters;
+module.exports.deleteUser = async (event) => {
+  const paramsID = event.pathParameters;
   let query = "delete from Contacts where ?";
   let sql = await mysql.query(query, paramsID);
   let response = {
@@ -95,4 +169,35 @@ module.exports.deleteOne = async (event) => {
     ),
   };
   return response;
+};
+
+module.exports.deleteOrg = async (event) => {
+  const paramsID = event.pathParameters;
+  let query = "delete from Organization where ?";
+  let response = {
+    statusCode:0,
+    body:""
+  }
+  mysql.query(query, paramsID,(err,res)=>{
+    if(err){
+      console.log("🚀 ~ file: api.js ~ line 183 ~ mysql.query ~ err", err.sqlMessage)
+      response.statusCode = 400,
+      response.body = JSON.stringify(err,null,2);
+      return response
+    }
+    console.log("🚀 ~ file: api.js ~ line 195 ~ mysql.query ~ res", res)
+    
+      response.statusCode =200,
+      response.body=JSON.stringify(
+        {
+          message: "Organization is deleted",
+          input: res,
+        },
+        null,
+        2
+      )
+    return response;
+  });
+  // console.log("🚀 ~ file: api.js ~ line 178 ~ module.exports.deleteOrg ~ sql", sql)
+  
 };
